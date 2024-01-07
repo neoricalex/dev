6d451aae98b74be320e85c34696a668561b31e45
neoricalex <neo.webmaster.2@gmail.com>
2024-01-07 17:16:09 +0100

    Geraçao da nfdos.iso OK

--- src/virtualhost/iniciar.sh
+++ src/virtualhost/iniciar.sh
@@ -107,6 +107,7 @@ instalar(){
       #    ruby-dev ruby-libvirt libxslt-dev libxml2-dev zlib1g-dev libvirt-dev zlib1g-dev
 
       vagrant plugin install vagrant-libvirt
+      vagrant plugin install vagrant-rsync-back
       #vagrant plugin install vagrant-mutate
     
     elif [ "$1" == "terraform" ]; then
@@ -144,7 +145,9 @@ iniciar(){
     if [ "$1" == "terraform" ]; then
         aviso "==> Iniciando o $1..."
         if [ ! -f ".terraform.lock.hcl" ]; then terraform init; fi
-        if [ ! -f "vps" ]; then 
+        if [ ! -f "vps" ]; then
+            # Trabalhar com workspaces: https://www.digitalocean.com/community/tutorials/how-to-deploy-multiple-environments-with-workspaces-in-your-terraform-project
+            terraform workspace new dev
             terraform plan -out=vps
             terraform apply "vps"
             aviso "==> Aguardando 15 segundos para que o $1 plan "vps" seja completamente aplicado e tenhamos um IP disponivel..."
@@ -165,7 +168,7 @@ iniciar(){
         aviso "==> Iniciando o $1..."
         sudo apt install -y net-tools sshpass
         ip_vps=$(for mac in `sudo virsh domiflist vps |grep -o -E "([0-9a-f]{2}:){5}([0-9a-f]{2})"` ; do sudo arp -e |grep $mac  |grep -o -P "^\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}" ; done)
-        sshpass -p neoricalex ssh -tt -o StrictHostKeyChecking=accept-new neoricalex@$ip_vps 'rm iniciar_nfdos*; wget https://raw.githubusercontent.com/neoricalex/dev/main/src/virtualhost/localhost/scripts/iniciar_nfdos.sh; bash iniciar_nfdos.sh'
+        sshpass -p neoricalex ssh -tt -o StrictHostKeyChecking=accept-new neoricalex@$ip_vps 'rm iniciar_nfdos*; wget https://raw.githubusercontent.com/neoricalex/dev/main/src/virtualhost/terraform/scripts/iniciar_nfdos.sh; bash iniciar_nfdos.sh'
         #sshpass -p neoricalex ssh -tt -o StrictHostKeyChecking=accept-new neoricalex@$ip_vps 'cd nfdos; git config --global user.email "you@example.com"; git config --global user.name "Your Name"; git add . ; git commit -m "Commit Automatico!"; git pull; bash compilar_nfdos.sh'
         #ssh -tt neoricalex@$ip_vps 'cd nfdos && sudo bash build.sh'
     fi
@@ -173,15 +176,41 @@ iniciar(){
 }
 checkar kvm
 checkar terraform
-cd localhost
-iniciar terraform
-#terraform destroy -auto-approve
-info "==> Aguardar 15 segundos para dar tempo ao VPS de iniciar completamente..."
-sleep 15
-iniciar vps
+checkar vagrant
+checkar packer
+
+cd packer
+if [ ! -f "img/ubuntu-22.04-server-cloudimg-amd64.img" ]; then 
+  wget http://cloud-images.ubuntu.com/releases/22.04/release/ubuntu-22.04-server-cloudimg-amd64.img -O img/ubuntu-22.04-server-cloudimg-amd64.img
+fi
+
+if [ ! -f "output/vps-v0.0.1.box" ]; then 
+  PACKER_LOG=1 PACKER_CACHE_DIR="./packer_cache/" packer build vps.pkr.hcl
+fi
 cd ..
+
+cd vagrant
+#bash criar_box_vagrant.sh /var/lib/libvirt/images/vps-vm-disk vps.box
+if [ ! -f "nfdos/builder/core/nfdos.iso" ]; then
+  rm -rf .cache
+  sudo rm -rf nfdos
+  rm .sudo_as_admin_successful
+  vagrant box add --force --name VPS --provider libvirt ../packer/output/vps-v0.0.1.box
+  vagrant up --provision
+  vagrant rsync-back
+fi
+cd ..
+
+cd terraform
+#iniciar terraform
+#info "==> Aguardar 15 segundos para dar tempo ao VPS de iniciar completamente..."
+#sleep 15
+#iniciar vps
+#terraform destroy -auto-approve
+
 #ssh-keygen -f "/home/neo/.ssh/known_hosts" -R "192.168.122.123"
 #sudo modprobe nbd max_part=1
-#sudo rmmod nbd
 #sudo qemu-nbd --connect=/dev/nbd0 /var/lib/libvirt/images/vps-vm-disk
 #sudo qemu-nbd --disconnect /dev/nbd0
+#sudo rmmod nbd
+cd ..
